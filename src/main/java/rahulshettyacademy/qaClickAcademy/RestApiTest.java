a94d5edd6b9cf0b3ec72805d61785e16eccbc7af
Yana <yana.mak89@gmail.com>
2023-11-05 12:12:39 +0100

    this is commit from Asia timezone guy

---
+++ src/main/java/rahulshettyacademy/qaClickAcademy/RestApiTest.java
@@ -0,0 +1,13 @@
+package rahulshettyacademy.qaClickAcademy;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+    @Test
+    public void postJira() {
+        System.out.println("postJira");
+        System.out.println("postJira2");
+        System.out.println("postJira3");
+
+    }
+}
